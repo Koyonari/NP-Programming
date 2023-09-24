7e85e5c80b42ac660c405696cc8d88f6cceeb793
Koyonari <104622557+Koyonari@users.noreply.github.com>
2023-09-24 17:09:48 +0800

    Delete Year 1/Exercises; directory

--- Year 1/Exercises;/Week 2/Activity9/activity9.py
+++
@@ -1,8 +0,0 @@
-#Read the file
-with open('StudentData.txt', 'r') as f:
-    contents = f.read()
-    contents = contents.splitlines()
-        #Loop to check line number
-    for i, line in enumerate(contents):
-            if i==0 or i==2 or i ==4:
-                print(line.strip())
